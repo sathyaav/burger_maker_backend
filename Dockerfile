8edf42789127fc97e63f5c9beb6d736039d3f322
Sathyaa <st630499@dal.ca>
2021-04-20 01:28:21 -0300

    Dockerrized

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:8-jdk-alpine
+FROM adoptopenjdk/openjdk11:jre-11.0.6_10-alpine
 RUN addgroup -S spring && adduser -S spring -G spring
 USER spring:spring
 ARG JAR_FILE=target/*.jar
--- src/main/java/com/cloud/burger/constants/KafkaConstants.java
+++ src/main/java/com/cloud/burger/constants/KafkaConstants.java
@@ -4,6 +4,6 @@ public class KafkaConstants {
 
 	public final static String KAFKA_TOPIC = "kafka-orders";
 	public static final String GROUP_ID = "kafka-burger";
-    public static final String KAFKA_BROKER = "localhost:9092";
+    public static final String KAFKA_BROKER = "burger_microservices:9092";
 
 }
